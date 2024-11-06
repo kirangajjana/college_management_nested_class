1eebf831a1d5e5f4d094721d84490df92d0e4932
kirangajjana <kiran.gajjana@gmail.com>
2024-11-06 23:08:17 +0530

    constructors added

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 #building an college management system basic functionalities using nested classes and nested method concepts
-class University:  #main class
-    pass
-    class Department:
-        pass
-        class Library:
-            pass
+class University:  #university Class
+    def __init__(self):
+        print("hello welcome to the university class")
+    class Department: #Department Class
+        def __init__(self):
+            print("hellop welcome to the department class")
+        class Library: #library Class
+            def __init__(self):
+                print("hello welcome to the library class")
 college=University().Department().Library()        
