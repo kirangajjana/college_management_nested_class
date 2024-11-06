02acfbf0b145759c6ce23983c9c169292f49a734
kirangajjana <kiran.gajjana@gmail.com>
2024-11-06 23:06:02 +0530

    added nested classes

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
 #building an college management system basic functionalities using nested classes and nested method concepts
+class University:  #main class
+    pass
+    class Department:
+        pass
+        class Library:
+            pass
+college=University().Department().Library()        
