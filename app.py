af9ac3d36f202dea74f331d0cae9e0a62bc6a168
kirangajjana <kiran.gajjana@gmail.com>
2024-11-06 23:12:07 +0530

    updated query

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 #building an college management system basic functionalities using nested classes and nested method concepts
 class University:  #university Class
-    def __init__(self,Department):
+    def __init__(self):
         print("hello welcome to the university class")
         self.department=self.Department()
     class Department: #Department Class
-        def __init__(self,Library):
+        def __init__(self):
             print("hellop welcome to the department class")
             self.library=self.Library()
         class Library: #library Class
