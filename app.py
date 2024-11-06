fa632d1c9fd99866d8a012203265e04da24e4204
kirangajjana <kiran.gajjana@gmail.com>
2024-11-06 23:10:49 +0530

    added nested methods

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 #building an college management system basic functionalities using nested classes and nested method concepts
 class University:  #university Class
-    def __init__(self):
+    def __init__(self,Department):
         print("hello welcome to the university class")
+        self.department=self.Department()
     class Department: #Department Class
-        def __init__(self):
+        def __init__(self,Library):
             print("hellop welcome to the department class")
+            self.library=self.Library()
         class Library: #library Class
             def __init__(self):
                 print("hello welcome to the library class")
-college=University().Department().Library()        
+college=University()
